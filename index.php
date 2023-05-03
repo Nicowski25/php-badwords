caaee9742a538d82e62dd0d3ede2f2d8b28c1544
Nicowski25 <nicola.faedo0610@gmail.com>
2023-05-03 15:46:11 +0200

    form

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <style>
+        form {
+            display: flex;
+            flex-direction: column;
+            width: 40%;
+        }
+    </style>
 </head>
 <body>
-    
+    <h1>Form</h1>
+    <form action="replace" method="post">
+        <label for="paragraph">Paragraph</label>
+        <input type="text" name="paragraph" placeholder="insert paragraph">
+        <label for="badword">Badword</label>
+        <input type="text" name="badword" placeholder="insert badword">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </body>
 </html>
